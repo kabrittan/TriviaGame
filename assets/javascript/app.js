41863c03161e5242d64c8f670802cb5186b41f17
Kyle Brittan <kabrittan@gmail.com>
2017-05-10 20:07:08 -0700

    Added JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+document.getElementById("form1").onsubmit=function() {
+       variable = parseInt(document.querySelector('input[name = "variable"]:checked').value);
+	   sub = parseInt(document.querySelector('input[name = "sub"]:checked').value);
+	   arr = parseInt(document.querySelector('input[name = "arr"]:checked').value);
+	   con = parseInt(document.querySelector('input[name = "con"]:checked').value);
+	   ifstate = parseInt(document.querySelector('input[name = "ifstate"]:checked').value);
+	   	   
+	   result = variable + sub + con + ifstate;
+	   
+	document.getElementById("grade").innerHTML = result;
+	   
+return false; // required to not refresh the page
+} //end of submission
