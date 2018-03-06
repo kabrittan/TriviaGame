77b91411a4e5b0f7eef59d266ca2c150534bdd06
Kyle Brittan <kabrittan@gmail.com>
2018-03-05 18:06:54 -0700

    Added Reset

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,15 +11,15 @@ document.getElementById("form1").onsubmit=function() {
  		
  		if (result == 0) {
 			 result2 = "You suck!<br /><img src='../images/0.jpg' width='300' />"};
-    	else if (result == 20) {
+    	if (result == 20) {
 			result2 = "I don't think you studied!<br /><img src='../images/20.jpg' width='300' />"};
-    	else if (result == 40) {
+    	if (result == 40) {
 			result2 = "You need to work harder - try again!<br /><img src='../images/40.jpg' width='300' />"};
-    	else if (result == 60) {
+    	if (result == 60) {
 			result2 = "I think you could do better - try again!<br /><img src='../images/60.jpg' width='300' />"};
-    	else if (result == 80) {
+    	if (result == 80) {
 			result2 = "So close - try again!<br /><img src='../images/80.jpg' width='300' />"};
-    	else if (result == 100) {
+    	if (result == 100) {
 			result2 = "Excellent! You're a JavaScript PRO!<br /><img src='../images/100.jpg' width='300' />"};
 
 	document.getElementById("grade2").innerHTML = result2;
