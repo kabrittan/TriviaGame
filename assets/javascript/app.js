1bd77262bc0a1f5ba9a5e0e6c0dd1d5382a698f9
Kyle Brittan <kabrittan@gmail.com>
2018-03-05 18:01:39 -0700

    Updated JS File

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,12 +9,18 @@ document.getElementById("form1").onsubmit=function() {
 	   
 	document.getElementById("grade").innerHTML = result;
  		
- 		if (result == 0) {result2 = "You suck!<br /><img src='../images/0.jpg' width='300' />"};
-    	if (result == 20) {result2 = "I don't think you studied!<br /><img src='../images/20.jpg' width='300' />"};
-    	if (result == 40) {result2 = "You need to work harder - try again!<br /><img src='../images/40.jpg' width='300' />"};
-    	if (result == 60) {result2 = "I think you could do better - try again!<br /><img src='../images/60.jpg' width='300' />"};
-    	if (result == 80) {result2 = "So close - try again!<br /><img src='../images/80.jpg' width='300' />"};
-    	if (result == 100) {result2 = "Excellent! You're a JavaScript PRO!<br /><img src='../images/100.jpg' width='300' />"};
+ 		if (result == 0) {
+			 result2 = "You suck!<br /><img src='../images/0.jpg' width='300' />"};
+    	else if (result == 20) {
+			result2 = "I don't think you studied!<br /><img src='../images/20.jpg' width='300' />"};
+    	else if (result == 40) {
+			result2 = "You need to work harder - try again!<br /><img src='../images/40.jpg' width='300' />"};
+    	else if (result == 60) {
+			result2 = "I think you could do better - try again!<br /><img src='../images/60.jpg' width='300' />"};
+    	else if (result == 80) {
+			result2 = "So close - try again!<br /><img src='../images/80.jpg' width='300' />"};
+    	else if (result == 100) {
+			result2 = "Excellent! You're a JavaScript PRO!<br /><img src='../images/100.jpg' width='300' />"};
 
 	document.getElementById("grade2").innerHTML = result2;
 
